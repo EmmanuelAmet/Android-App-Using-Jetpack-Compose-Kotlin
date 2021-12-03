00fc8deeb6f397746730579a1880e352c6065b91
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2021-12-03 10:48:14 -0800

    Column added.

--- app/src/main/java/com/emmanuel/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/emmanuel/jetpack_compose/MainActivity.kt
@@ -3,10 +3,18 @@ package com.emmanuel.jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.emmanuel.jetpack_compose.ui.theme.Jetpack_composeTheme
 
@@ -14,11 +22,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Jetpack_composeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            Column(modifier = Modifier
+                .fillMaxHeight(0.5f)
+                .background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly) {
+                Text("Rose")
+                Text("Florence")
             }
         }
     }
